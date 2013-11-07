66954030d5a9a7e0952912e4438dcbc07c142973
Oleg <24privat@users.noreply.github.com>
2013-11-07 11:45:45 +0200

    Create gurmans.php

---
+++ gurmans.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>GURMANS</title>
+<meta name="keywords" content="вкусная еда, вегитареанство, вегетарианство,здоровье,вкусно,
+                               клетчатка, натуральные масла, растительные масла, каши, салаты, 
+                               первое, второе, кампот, салат, борщ,мясо, овощи, вкусно,
+                               быстро вкусно, купить масла растительные, купить клетчатку,
+                               купить кашу, купить полуфабрикат, опт, розница клетчатка, опт
+                               розница каша, опты розница масла, цены от производителя, 
+                               украинский производитель, купить он лайн, gurmans,kletchatka,maslo,opt,
+                               kupitj zakaz," />
+
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<?php include_once "analyticstracking.php"; ?>
+<?php require_once "all_files.php"; ?>
+
+</body>
+</html>
