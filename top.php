8a23686d81593c21a6ee056c9021bc30db6a3531
Oleg <24privat@users.noreply.github.com>
2013-11-15 09:42:21 +0200

    Create top.php

---
+++ top.php
@@ -0,0 +1,14 @@
+<div id="ipg_header">
+        <div id="site_title"></div>
+</div> <!-- end of header -->
+
+    <div id="menu">
+        <ul>
+            <li class="current"><a href="gurmans.php"  target="_blank"><b>Главная</b></a></li>
+            <li><a href="gurmans.php"  target="_blank"><b>Вегетарианство</b></a></li>
+              <li><a href="gurmans.php" target="_blank"><b>Топ вкусностей</b></a></li>
+              <li><a href="gurmans.php" target="_blank"><b>Новое в меню</b></a></li>
+            <li><a href="pages/vazno_znatj.php" target="_blank"><b>Важно знать!!!</b></a></li>
+            <li><a href="http://www.gurmans.in.ua/guest" target="_blank"><b>Гостевая книга</b></a></li>
+        </ul>
+    </div>
