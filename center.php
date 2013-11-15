0b5db2889fdb2df5a478deb05ffc521d44e7921d
Oleg <24privat@users.noreply.github.com>
2013-11-15 09:41:43 +0200

    Create center.php

---
+++ center.php
@@ -0,0 +1,194 @@
+
+    <div id="ipg_top_dishes">
+
+        <h1>Сайт о вкусной и здоровой пище...</h1>
+
+     <div class="top_dishes_box">
+             <?php
+                $pervie_blyuda[0]='images/pervie_blyuda_1.png';
+                $pervie_blyuda[1]='images/pervie_blyuda_2.png';
+                $pervie_blyuda[2]='images/pervie_blyuda_3.png';
+                $pervie_blyuda[3]='images/pervie_blyuda_4.png';
+                $pervie_blyuda[4]='images/pervie_blyuda_5.png';
+                $i=rand(0,4); 
+                echo '<a href="pages/pervie_blyuda.php" target="_blank"><img src="'.$pervie_blyuda[$i].'" /></a>';
+              ?>
+             
+            <h2>Первые блюда</h2>
+          <p>Для приготовления  <a href="http://www.ipg.in.ua" target="_blank">Все рацепты...</a></p>
+      </div>
+
+
+      <div class="top_dishes_box">
+             <?php
+                $vtorie_blyuda[0]='images/vtorie_blyuda_1.png';
+                $vtorie_blyuda[1]='images/vtorie_blyuda_2.png';
+                $vtorie_blyuda[2]='images/vtorie_blyuda_3.png';
+                $vtorie_blyuda[3]='images/vtorie_blyuda_4.png';
+                $vtorie_blyuda[4]='images/vtorie_blyuda_5.png';
+                $vtorie_blyuda[5]='images/vtorie_blyuda_6.png';
+                $i=rand(0,5); 
+                echo '<a href="pages/vtorie_blyuda.php" target="_blank"><img src="'.$vtorie_blyuda[$i].'" /></a>';
+              ?>
+          <h2>Вторые блюда</h2>
+            <p>Для приготовления  <a href="http://www.ipg.in.ua" target="_blank">Все рацепты...</a></p>
+      </div>
+
+        <div class="top_dishes_box">
+             <?php
+                $zakuski[0]='images/zakuski_1.png';
+                $zakuski[1]='images/zakuski_2.png';
+                $zakuski[2]='images/zakuski_3.png';
+                $zakuski[3]='images/zakuski_4.png';
+                $zakuski[4]='images/zakuski_5.png';
+                $i=rand(0,4); 
+                echo '<a href="pages/zakuski.php" target="_blank"><img src="'.$zakuski[$i].'" /></a>';
+              ?>
+            <h2>Закуски</h2>
+          <p>Для приготовления  <a href="http://http://www.ipg.in.ua" target="_blank">Все рацепты...</a></p>
+        </div>
+
+
+        <div class="top_dishes_box">
+             <?php
+                $salat[0]='images/salat_1.png';
+                $salat[1]='images/salat_2.png';
+                $salat[2]='images/salat_3.png';
+                $salat[3]='images/salat_4.png';
+                $salat[4]='images/salat_5.png';
+                $salat[5]='images/salat_6.png';
+                $i=rand(0,5); 
+                echo '<a href="pages/salat.php" target="_blank"><img src="'.$salat[$i].'" /></a>';
+              ?>
+            <h2>Салаты</h2>
+          <p>Для приготовления <a href="http://www.ipg.in.ua" target="_blank">Все рацепты...</a></p>
+        </div>
+
+        <div class="top_dishes_box">
+             <?php
+                $desert[0]='images/desert_1.png';
+                $desert[1]='images/desert_2.png';
+                $desert[2]='images/desert_3.png';
+                $desert[3]='images/desert_4.png';
+                $desert[4]='images/desert_5.png';
+                $i=rand(0,4); 
+                echo '<a href="pages/desert.php" target="_blank"><img src="'.$desert[$i].'" /></a>';
+              ?>
+            <h2>Десерты</h2>
+          <p>Для приготовления  <a href="http://www.ipg.in.ua" target="_blank">Все рацепты...</a></p>
+        </div>
+
+        <div class="top_dishes_box">
+             <?php
+                $napitki[0]='images/napitki_1.png';
+                $napitki[1]='images/napitki_2.png';
+                $napitki[2]='images/napitki_3.png';
+                $napitki[3]='images/napitki_4.png';
+                $napitki[4]='images/napitki_5.png';
+                $i=rand(0,4); 
+                echo '<a href="pages/napitki.php" target="_blank"><img src="'.$napitki[$i].'" /></a>';
+              ?>
+            <h2>Напитки</h2>
+          <p>Для приготовления  <a href="http://www.ipg.in.ua" target="_blank">Все рацепты...</a></p>
+        </div>
+
+        <div class="top_dishes_box">
+              <?php
+                $veg[0]='images/veg_1.png';
+                $veg[1]='images/veg_2.png';
+                $veg[2]='images/veg_3.png';
+                $veg[3]='images/veg_4.png';
+                $veg[4]='images/veg_5.png';
+                $i=rand(0,4); 
+                echo '<a href="pages/veg.php" target="_blank"><img src="'.$veg[$i].'" /></a>';
+              ?>
+            <h2>Вегетарианские блюда</h2>
+          <p>Для приготовления  <a href="http://www.ipg.in.ua" target="_blank">Все рацепты...</a></p>
+        </div>
+
+
+        <div class="top_dishes_box">
+             <?php
+                $poleznost[0]='images/poleznost_1.png';
+                $poleznost[1]='images/poleznost_2.png';
+                $poleznost[2]='images/poleznost_3.png';
+                $poleznost[3]='images/poleznost_4.png';
+                $poleznost[4]='images/poleznost_5.png';
+                $i=rand(0,4); 
+                echo '<a href="pages/poleznost.php" target="_blank"><img src="'.$poleznost[$i].'" /></a>';
+              ?>
+            <h2>О полезности продуктов</h2>
+          <p>Тут Вы узнаете какой продукт полезный, а какой нет, а так же <a href="vazno_znatj.html" target="_blank">много всего интересного...</a></p>
+        </div>
+
+        <div class="top_dishes_box">
+             <?php
+               $masla[0]='images/masla_1.png';
+               $masla[1]='images/masla_2.png';
+               $masla[2]='images/masla_3.png';
+               $masla[3]='images/masla_4.png';
+               $masla[4]='images/masla_5.png';
+               $i=rand(0,4); 
+               echo '<a href="pages/masla.php" target="_blank"><img src="'.$masla[$i].'" /></a>';
+             ?>
+            <h2>Натуральные масла</h2>
+          <p>Тут Вы найдете интересные статьи о различных растительных маслах их полезности, а так же <a href="pages/masla.php" target="_blank">много всего интересного...</a></p>
+        </div>
+        
+        
+        <div class="top_dishes_box">
+             <?php
+                $kletchatka[0]='images/kletchatka_1.png';
+                $kletchatka[1]='images/kletchatka_2.png';
+                $kletchatka[2]='images/kletchatka_3.png';
+                $kletchatka[3]='images/kletchatka_4.png';
+                $kletchatka[4]='images/kletchatka_5.png';
+                $i=rand(0,4); 
+                echo '<a href="pages/kletchatka.php" target="_blank"><img src="'.$kletchatka[$i].'" /></a>';
+              ?>
+            <h2>Клетчатка</h2>
+          <p>Тут Вы найдете интересные статьи о натуральной клетчатке, полезности отрубей, а так же <a href="kletchatka.php" target="_blank">много всего интересного...</a></p>
+        </div>
+        
+        <div class="top_dishes_box">
+             <?php
+                $kasha[0]='images/kasha_1.png';
+                $kasha[1]='images/kasha_2.png';
+                $kasha[2]='images/kasha_3.png';
+                $kasha[3]='images/kasha_4.png';
+                $kasha[4]='images/kasha_5.png';
+                $i=rand(0,4); 
+                echo '<a href="pages/kasha.php" target="_blank"><img src="'.$kasha[$i].'" /></a>';
+              ?>
+            <h2>Каши</h2>
+          <p>Тут Вы найдете интересные статьи о миксах каш, каллориях, полезности диет, а так же <a href="pages/kasha.php" target="_blank">много всего интересного...</a></p>
+        </div>
+        
+        
+        <div class="top_dishes_box">
+             <a href="pages/vazno_znatj.php" target="_blank"><img src="images/8.png" alt="image" /></a>
+            <h2>Важно знать!!!</h2>
+          <p>Тут Вы найдете интересные статьи о продуктах, каллориях, полезности диет, а так же <a href="vazno_znatj.php" target="_blank">много всего интересного...</a></p>
+        </div>
+        
+        <div class="vijet">
+                                     <script src="http://partner.privatbank.ua/wv1.js" 
+                                                type="text/javascript" 
+                                                charset="UTF-8" 
+                                                partner_main_id="9861693969" 
+                                                chanalname="null" 
+                                                chanalcomment="null"></script> 
+              <h2>Виртуальное отделение ПАТ Приватбанка</h2>               
+                      
+        </div>
+        
+         <div class="vijet">
+                                     <iframe src="http://agent.privatbank.ua/gadget/54467/" height="266px" width="235px" frameborder="0" scrolling="no"> </iframe>                
+                      
+        </div>
+
+        <div class="cleaner">&nbsp;</div>
+
+    </div>
+    
+     
