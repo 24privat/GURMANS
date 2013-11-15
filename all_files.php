a542750f7d4c8b0bbaf38f6a5e37175d63318f1b
Oleg <24privat@users.noreply.github.com>
2013-11-15 09:43:46 +0200

    Create all_files.php

---
+++ all_files.php
@@ -0,0 +1,13 @@
+
+<div id="ipg_container">
+
+<?php require_once "top.php"; ?>
+<!-- end of menu -->
+
+<?php require_once "center.php"; ?>
+<!-- end of top dishes (center) -->
+
+<?php require_once "footer.php" ?>
+
+</div>
+<!-- end of container -->
