104c282f82297decf6ab931702f7fc176e36b863
Oleg <24privat@users.noreply.github.com>
2013-11-15 09:44:35 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,51 @@
+    <div id="ipg_content">
+        <span class="top"></span>
+        <div id="ipg_innter_content">
+
+                <div id="ipg_content_left">
+                    <h1>Добро пожаловать на сайт!</h1>
+                <?php
+                $left_footer[0]='images/masla_4.png';
+                $left_footer[1]='images/kletchatka_4.png';
+                $left_footer[2]='images/masla_4.png';
+                $left_footer[3]='images/ipg_image_05.jpg';
+                $left_footer[4]='images/masla_4.png';
+                $i=rand(0,4); 
+                echo '<a href="pages/kasha.php" target="_blank"><img src="'.$left_footer[$i].'" /></a>';
+              ?>
+                    <p>Вашему вниманию представлены самые удобные и вкусные рецепты. <a href="href="http://www.ipg.in.ua" target="_blank">Читать далее...</a></p>
+                  <p>Отдельное внимение хотелось бы уделить обсуждениям и статьям о здоровом образе жизни. <a href="href="http://www.ipg.in.ua" target="_blank">Читать далее...</a></p>
+                  <p>Самые важные для нас продукуты.<a href="href="http://www.ipg.in.ua" target="_blank">Читать далее...</a></p>
+
+                    <div style="clear: both; padding: 30px 0 20px 0;"> 
+                <a href="http://www.ipg.in.ua"><img style="border:0;width:88px;height:31px" src="http://www.ipg.in.ua" alt="Valid XHTML 1.0 Transitional" width="88" height="31" vspace="8" border="0" /></a>
+                <a href="http://www.ipg.in.ua"><img style="border:0;width:88px;height:31px"  src="http://www.ipg.in.ua" alt="Valid CSS!" vspace="8" border="0" /></a>
+                    </div>
+
+                    <div class="cleaner_with_height">&nbsp;</div>
+
+                </div> <!-- end of content left -->
+
+                <div id="ipg_content_right">
+                    <h1>Что нового?</h1>
+                    <div class="right_column_section">
+                        <h2>Специально для Вас</h2>
+                        <img src="images/ipg_image_06.jpg" alt="image" />
+                        <p>Если хотите поделиться рецептом с нами ждем Вас на нашей </p>
+                        <a href="#">Гостевой странице...</a>
+                     </div>
+
+                </div> <!-- end of content right -->
+                <div class="cleaner">&nbsp;</div>
+        </div>
+        <div class="cleaner" style="background: #fff;">&nbsp;</div>
+    </div>
+
+
+<div id="ipg_footer">
+
+        <a href="http://www.ipg.in.ua">Главная</a> | <a href="http://www.ipg.in.ua">Вегитарианство</a> | <a href="http://www.ipg.in.ua">Топ вкусностей</a> | <a href="http://www.ipg.in.ua">Новое в меню</a> | <a href="http://www.ipg.in.ua" target="_blank>Важные статьи</a> | <a href="http://www.ipg.in.ua">Гостевая книга</a><br />
+        IPG <a href="http://www.ipg.in.ua"><strong>Internet Page for Gurman's</strong></a> | <a href="http://www.ipg.in.ua" target="_parent">IPG</a> by <a href="http://www.ipg.in.ua" title="Internet Page for Gurman's" target="_parent">Internet Page for Gurman's</a></div> <!-- end of footer -->
+<!--  Бесплатные рецепты и статьи о еде  -->
+</div>
+</div>
